55fdffbad36e85414726a0e48456f15234274fae
markthree <1801982702@qq.com>
2022-02-11 13:41:13 +0800

    chore: init plugin

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,14 @@
-import { resolve } from 'path'
-const foo = 1
+import { Plugin, normalizePath } from 'vite'
+import { createPluginName } from './shared/create'
 
-export { resolve, foo }
+interface Options {}
 
+const useName = createPluginName()
+
+const usePlugin = (options: Partial<Options>): Plugin => {
+	return {
+		name: useName('name')
+	}
+}
+
+export default usePlugin
